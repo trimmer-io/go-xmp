1819159aa97dfb5264f08aa52629a17216e9e5a2
Alexander Eichhorn <alex@kidtsunami.com>
2018-03-05 18:36:37 +0100

    sync before listing path

--- xmp/path.go
+++ xmp/path.go
@@ -975,6 +975,10 @@ func SetModelPath(v Model, path Path, value string, flags SyncFlags) error {
 }
 
 func (d *Document) ListPaths() (PathValueList, error) {
+	// sync individual models to establish correct XMP entries
+	if err := d.syncXMP(); err != nil {
+		return err
+	}
 	l := make(PathValueList, 0)
 	for _, v := range d.nodes {
 		if v.Model != nil {
